6178a59f039b18efb8bf7e6a55b62961bf9d5097
remincd <remin-p@proton.me>
2024-06-29 13:17:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import itemListA from './components/itemListA'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          As it turns out, I don't know what I'm doing...
+        </p>
+      </header>
+      <itemListA />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/itemListA/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function itemListA() {
+  return (
+    <div>
+      <p>itemListA</p>
+    </div>
+  )
+}
+
+export default itemListA
